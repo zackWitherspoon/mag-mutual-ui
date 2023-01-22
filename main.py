58ad5708b100b48cc3ac179b0220e7d8ccb39d34
Zack Witherspoon <39570323+zackWitherspoon@users.noreply.github.com>
2023-01-22 01:50:33 -0500

    Updated main.py with final calls. Grooming still left and README.md

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import datetime
+import logging as log
 import requests
 import streamlit as st
 
@@ -11,36 +12,66 @@ def fetch(session, url):
         return {}
 
 
+def fetch_post(session, url, body):
+    try:
+        result = session.post(url, json=body)
+        print(result)
+        return result.json(), result.status_code
+    except Exception as err:
+        print('ERROR!!!!!')
+        print(err)
+        return st.error(err), 400
+
+
 def main():
     st.title("Mag Mutual UI")
     session = requests.Session()
 
-    url = 'https://www.streamlit.io/'
+    url = 'http://localhost:10000/users'
     if st.button('Clear'):
         st.write('')
     with st.form("Get All Users"):
+        st.title("Get All Users")
         submitted = st.form_submit_button('Get All Users')
         if submitted:
-            data = fetch(session, f"http://localhost:10000/users")
+            data = fetch(session, f"{url}")
             if data:
                 st.table(data['users'])
             else:
                 st.error("Error")
     with st.form("Get Users by Date"):
-        st.title("GetUsers by Date")
-        fromDate = st.date_input("When\'s fromDate birthday", datetime.date(2019, 7, 6))
-        toDate = st.date_input("When\'s toDate birthday", datetime.date(2019, 7, 6))
-        st.write('fromDate  & toDate:', fromDate, toDate)
+        st.title("Get Users by Date")
+        fromDate = st.date_input("When\'s fromDate birthday", datetime.date(2022, 2, 16))
+        toDate = st.date_input("When\'s toDate birthday", datetime.date(2022, 2, 25))
         submitted = st.form_submit_button("Submit")
         if submitted:
-           st.write("Result")
-           data = fetch(session, f"http://localhost:10000/users/byDate?fromDate={fromDate}&toDate={toDate}")
-           if data:
-              st.table(data['users'])
-           else:
-              st.error("Error")
-    st.button('Get Users by ID')
-    st.button('Get Users by Profession')
+            data = fetch(session, f"{url}/byDate?fromDate={fromDate}&toDate={toDate}")
+            if data:
+                st.table(data['users'])
+            else:
+                st.error(data.reason)
+    with st.form("Get Users by Id"):
+        st.title("Get Users by Id")
+        user_id = st.number_input('Insert a number', value=100)
+        submitted = st.form_submit_button("Submit")
+        if submitted:
+            data = fetch(session, f"{url}/id/{user_id}")
+            if data:
+                st.table(data['users'])
+            else:
+                st.error(data.reason)
+    with st.form("Get Users by Profession"):
+        st.title("Get Users by Profession")
+        profession = st.text_input('Insert a number', "firefighter")
+        submitted = st.form_submit_button("Submit")
+        if submitted:
+            json_data = {"profession": profession}
+            print(json_data)
+            data, status_code = fetch_post(session, f"{url}/byProfession", json_data)
+            if data:
+                st.table(data['users'])
+            else:
+                st.error(status_code)
 
 
 if __name__ == '__main__':
