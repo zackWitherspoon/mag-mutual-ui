fc9d37ad070c8b27adfd70e9530e263ddd82e3a2
zackWitherspoon <frisbex@gmail.com>
2023-01-22 16:05:06 -0500

    Added final touches to the python project

--- main.py
+++ main.py
@@ -3,63 +3,67 @@ import logging as log
 import requests
 import streamlit as st
 
+url = 'http://localhost:10000/users'
+
 
 def fetch(session, url):
     try:
         result = session.get(url)
-        return result.json()
-    except Exception:
-        return {}
+        if result.status_code == 200:
+            return result.json(), None
+        else:
+            return {}, result.text
+    except Exception as err:
+        log.error(err)
+        return {}, err
 
 
 def fetch_post(session, url, body):
     try:
         result = session.post(url, json=body)
         print(result)
-        return result.json(), result.status_code
+        return result.json(), None
     except Exception as err:
-        print('ERROR!!!!!')
-        print(err)
-        return st.error(err), 400
+        log.error(err)
+        return {}, err
 
 
 def main():
     st.title("Mag Mutual UI")
     session = requests.Session()
 
-    url = 'http://localhost:10000/users'
     if st.button('Clear'):
         st.write('')
     with st.form("Get All Users"):
         st.title("Get All Users")
         submitted = st.form_submit_button('Get All Users')
         if submitted:
-            data = fetch(session, f"{url}")
+            data, err = fetch(session, f"{url}")
             if data:
                 st.table(data['users'])
             else:
-                st.error("Error")
+                st.error(err)
     with st.form("Get Users by Date"):
         st.title("Get Users by Date")
-        fromDate = st.date_input("When\'s fromDate birthday", datetime.date(2022, 2, 16))
-        toDate = st.date_input("When\'s toDate birthday", datetime.date(2022, 2, 25))
+        from_date = st.date_input("When\'s fromDate birthday", datetime.date(2022, 2, 16))
+        to_date = st.date_input("When\'s toDate birthday", datetime.date(2022, 2, 25))
         submitted = st.form_submit_button("Submit")
         if submitted:
-            data = fetch(session, f"{url}/byDate?fromDate={fromDate}&toDate={toDate}")
+            data, err = fetch(session, f"{url}/byDate?fromDate={from_date}&toDate={to_date}")
             if data:
                 st.table(data['users'])
             else:
-                st.error(data.reason)
+                st.error(err)
     with st.form("Get Users by Id"):
         st.title("Get Users by Id")
         user_id = st.number_input('Insert a number', value=100)
         submitted = st.form_submit_button("Submit")
         if submitted:
-            data = fetch(session, f"{url}/id/{user_id}")
+            data, err = fetch(session, f"{url}/id/{user_id}")
             if data:
                 st.table(data['users'])
             else:
-                st.error(data.reason)
+                st.error(err)
     with st.form("Get Users by Profession"):
         st.title("Get Users by Profession")
         profession = st.text_input('Insert a number', "firefighter")
@@ -67,11 +71,11 @@ def main():
         if submitted:
             json_data = {"profession": profession}
             print(json_data)
-            data, status_code = fetch_post(session, f"{url}/byProfession", json_data)
+            data, err = fetch_post(session, f"{url}/byProfession", json_data)
             if data:
                 st.table(data['users'])
             else:
-                st.error(status_code)
+                st.error(err)
 
 
 if __name__ == '__main__':
